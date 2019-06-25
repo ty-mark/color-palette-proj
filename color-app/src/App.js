abf78403cca0bf3a9bd95d0f2670d87bea48d602
ty-mark <thyang@utexas.edu>
2019-06-24 21:31:55 -0500

    add unstyled color slider

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -5,10 +5,9 @@ import { generatePalette } from './colorHelpers';
 
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[1]));
     return (
       <div>
-        <Palette {...seedColors[3]} />
+        <Palette palette={generatePalette(seedColors[3])} />
       </div>
     );
   }
