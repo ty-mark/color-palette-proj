b0661f13fe1f1d1bddac15467a7dcba8a0d80c98
ty-mark <thyang@utexas.edu>
2019-06-26 22:42:57 -0500

    add homepage and routes to each color scheme

--- color-app/src/App.js
+++ color-app/src/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PaletteList from './PaletteList';
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette(id) {
+    /* Returns the palette which matches the request id */
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
   render() {
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[0])} />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={() => <PaletteList palettes={seedColors} />}
+        />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+      </Switch>
     );
   }
 }
