199c188ce47cc52baddca7340b0680c59872d973
ty-mark <thyang@utexas.edu>
2019-06-30 12:14:10 -0500

    add go-back button and styles at single color palette

--- color-app/src/PaletteList.js
+++ color-app/src/PaletteList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
 import MiniPalette from './MiniPalette';
-import { Link } from 'react-router-dom';
 
 const styles = {
   root: {
